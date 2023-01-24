b55151981e5411013008b777e21a7ac5bca9ac57
Ryan Fu <fuzhaoyuan@gmail.com>
2023-01-24 11:50:59 -0500

    Spring Initializr

---
+++ src/main/java/com/leafstudio/goodtodo/GoodTodoApplication.java
@@ -0,0 +1,13 @@
+package com.leafstudio.goodtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leafstudio/goodtodo/GoodTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leafstudio.goodtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
